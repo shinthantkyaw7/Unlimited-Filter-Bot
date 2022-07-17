a75370329993362f89fa471d58d0d1eadb06edbc
Shin Thant kyaw <109136650+shinthantkyaw7@users.noreply.github.com>
2022-07-17 09:30:16 +0630

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Channel", url="https://t.me/+XZpJfRc0kDkxZmJl"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Owner", url="https://t.me/battlelorian"),
+                    InlineKeyboardButton("Group", url="https://t.me/+DwFx3p9owBdmM2I9")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/+XZpJfRc0kDkxZmJl")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
