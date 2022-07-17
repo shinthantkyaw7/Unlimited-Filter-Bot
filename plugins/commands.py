f9a0a7430cabce656fcac427568af66780d3afc6
Shin Thant kyaw <109136650+shinthantkyaw7@users.noreply.github.com>
2022-07-17 09:09:31 +0630

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy?", url="https://t.me/+XZpJfRc0kDkxZmJl"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/Channel_X_Movies"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/+XZpJfRc0kDkxZmJl")
                 ]
             ]
         ),
